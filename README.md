458c9c5ab4786c5cd00157b220e4ea12ba04130a
Sheila Nery <sheila.sane@gmail.com>
2021-08-20 22:18:19 -0300

    Envio desafio

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Repositório criado referente ao curso Cappacita - programação de IA conversacional - Chatbot 🤖
 
-
+### Exercícios feitos durante o curso e o desafio: Criar uma rota para curar o Pokemon com porções.
--- desafio_modulo3/src/server.js
+++ desafio_modulo3/src/server.js
@@ -44,20 +44,8 @@ app.post('/batalha', (req, res) => {
     res.send(dataBase.batalhaPokemon(req.body.id1, req.body.id2))
 })
 
-
 app.put('/curar/:id', (req, res) => {
-
-    const pokemon = dataBase.curarPokemon(req.params.id, {
-        nome: req.body.nome,
-        tipo: req.body.tipo,
-        fraqueza: req.body.fraqueza,
-        resistencia: req.body.resistencia,
-        hp: 100,
-        id: parseInt(req.params.id)
-    })
-    res.send(pokemon)
+    res.send(dataBase.curarPokemon(req.params.id))
 })
-
-
 
 app.listen(3003)
